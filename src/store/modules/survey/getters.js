ae3be1a2519522329f083c1346d62318596471e0
Conor Leech <conor.leech@argos.co.uk>
2020-11-28 13:57:19 +0000

    success layout added

--- src/store/modules/survey/getters.js
+++ src/store/modules/survey/getters.js
@@ -1,3 +1,11 @@
+const addPadding = (number) => {
+  return number > 9 ? number : `0${number}`
+}
+
+const isValidDate = (date) => {
+  return date instanceof Date && !isNaN(date)
+}
+
 export default {
   getName: (state) => state.name,
   getGoals: (state) => {
@@ -6,5 +14,12 @@ export default {
   getDiet: (state) => {
     return Object.entries(state.diet).map(([key, value]) => value.name)[0] || ''
   },
-  getDob: (state) => state.dob
+  getDob: (state) => state.dob,
+  getFormattedGoals: (state, getters) => {
+    return getters.getGoals.join(', ')
+  },
+  getFormattedDob: (state, getters) => {
+    const date = new Date(getters.getDob)
+    return isValidDate(date) ? `${addPadding(date.getDate())}/${addPadding(date.getMonth() + 1)}/${date.getFullYear()}` : ''
+  }
 }
